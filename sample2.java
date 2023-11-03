0e41110058ef25edfd0ed32377266107a4a7946f
mallikarjuna <mallikarjuna@gmail.com>
2023-11-03 16:22:07 +0530

    edition 3

--- sample2.java
+++ sample2.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins from sample.java file");
+        System.out.println("Hello all this is from sample 2");
     }
 }
